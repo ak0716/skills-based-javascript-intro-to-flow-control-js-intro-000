69e91a61453a396bdbe383367eb738f07659e2df
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-03 23:28:42 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -11,8 +11,10 @@ function teenager(age) {
   return 'You are not a teenager';
 }
 
-function ageChecker(age) {
-  if (age <= 12) {
+function ageChecker(age) { if (age >= 13 && age <= 19) {
+  return 'You are a teenager'
+}
+  else if (age < 13) {
     return 'You are a kid'
   }
   else if (age >= 20) {
